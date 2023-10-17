970b4c8c0d345965767119f25f2370a2ff338cf6
nhinojosa <nhinojosa@southtexascollege.edu>
2023-10-17 08:08:11 -0500

    corrected router.js for triviantastic

--- src/views/QuestionPage.vue
+++ src/views/QuestionPage.vue
@@ -24,7 +24,7 @@ const handleAnswer = (points) => {
   showNotification.value = true
   setTimeout(() => {
     changeScore(points)
-    router.push('/')
+    router.push('/triviantastic/')
   }, 1000)
 }
 
